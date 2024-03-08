5186d902a2beeecb14cbb7b18173c8a47df87043
AlexandraAiloae <ailoae.alexandra@yahoo.com>
2024-03-08 14:43:49 +0200

    Fixed Dynamic Error

--- app/api/filterproduct/route.ts
+++ app/api/filterproduct/route.ts
@@ -1,22 +1,23 @@
 import prisma from "@/app/prismadb";
 import { NextResponse } from "next/server";
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
   try {
-    const searchParams = new URLSearchParams(request.url.split('?')[1]);
+    const searchParams = new URLSearchParams(request.url.split("?")[1]);
 
     const categories = searchParams.getAll("categories[]");
 
-    const products = categories.length > 0
-      ? await prisma.product.findMany({
-          where: {
-            category: {
-              in: categories,
+    const products =
+      categories.length > 0
+        ? await prisma.product.findMany({
+            where: {
+              category: {
+                in: categories,
+              },
             },
-          },
-        })
-      : await prisma.product.findMany();
+          })
+        : await prisma.product.findMany();
 
     return NextResponse.json({ message: "API route is working", products });
   } catch (error) {
